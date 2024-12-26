f033a2c3a57776ee064b1e1f648f9e04f9beeaad
joker <joe.flores.pro@gmail.com>
2024-12-25 23:24:54 -0600

    fix the content

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,189 +2,189 @@
 
 const CONFIG = {
   github: {
-    username: 'smartjoker0117', // Your GitHub org/user name. (This is the only required config)
+    username: 'smartjoker0117',
   },
-  /**
-   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/smartjoker0117/smartjoker0117.github.io, set base to '/'.
-   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/smartjoker0117/portfolio, then set base to '/portfolio/'.
-   */
+
   base: '/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
+      display: true,
       header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      mode: 'automatic',
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 8,
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['smartjoker0117/my-project1', 'smartjoker0117/my-project2']
+          forks: false,
+          projects: [],
         },
       },
       manual: {
-        // Properties for manually specifying projects
-        projects: ['smartjoker0117/gitprofile', 'smartjoker0117/pandora'], // List of repository names to display. example: ['smartjoker0117/my-project1', 'smartjoker0117/my-project2']
+        projects: ['smartjoker0117/gitprofile', 'smartjoker0117/pandora'],
       },
     },
     external: {
       header: 'My Projects',
-      // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          title: `Chewy Platform`,
+          description: `Contributed to the development and maintenance of Chewy Platform projects including Internal UI Library, Customer Service Dashboard, and so on.`,
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://imgcdn.stablediffusionweb.com/2024/9/7/051ae04d-1d62-46d9-91ce-f66ea7e12aa6.jpg',
+          link: 'https://www.chewy.com',
         },
         {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          title: `HealthMine HRA`,
+          description: `Updated HealthMine's Health Risk Assessment (HRA) project improving COVID-19-related data handling and user experience during the pandemic.`,
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://thumbs.dreamstime.com/b/health-risk-assessment-concept-icon-health-risk-assessment-concept-icon-patient-diagnostics-checkup-disease-family-doctor-213638838.jpg',
+          link: 'https://www.healthmine.com/digital-hra',
+        },
+        {
+          title: `Popcorn and Chill`,
+          description: `Users can see which movies are popular currently, search and enjoy movies based on their ratings, title, actor, or other details by simple one click.`,
+          imageUrl:
+            'https://pics.craiyon.com/2024-01-26/I_Mm_hWZQUuC7f8F4PautQ.webp',
+          link: 'https://github.com/smartjoker0117/popcorn-and-chill',
+        },
+        {
+          title: `News App - React Native`,
+          description: `Responsive mobile app where users can browse today's news with a smart and responsive interface - Personal project using React Native`,
+          imageUrl:
+            'https://static.vecteezy.com/system/resources/previews/016/817/592/non_2x/phone-live-news-icon-outline-smart-online-vector.jpg',
+          link: 'https://github.com/smartjoker0117/react-native-news-app',
         },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
-    description: '',
-    imageURL: '',
+    title: 'Portfolio of Jarrod Henry',
+    description:
+      'Jarrod Henry | Senior Software Engineer | Full Stack | 10+ Years | JavaScript | TypeScript | React | Node.js | Java | Python | C# | .NET | Irving, Texas, United States | Remote',
+    imageURL:
+      'file:///D:/Devel/Portfolio/smartjoker0117.github.io/public/logo.png',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: 'jarrod-henry-link',
+    twitter: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
     reddit: '',
     threads: '',
-    youtube: '', // example: 'pewdiepie'
+    youtube: '',
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
+    medium: '',
+    dev: '',
+    stackoverflow: '',
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    website: '',
+    phone: '+1 (404) 448-1373',
+    email: 'jarrod.henry.pro@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1GVsduu1nSKirqhrKAQRoeypwyS6FCzTG/view?usp=drive_link',
   },
   skills: [
-    'PHP',
-    'Laravel',
     'JavaScript',
+    'TypeScript',
     'React.js',
     'Node.js',
-    'Nest.js',
+    'Java',
+    'Python',
+    'C#',
+    'RESTful API',
+    'GraphQL',
+    'FastAPI',
+    'MongoDB',
     'MySQL',
+    'SQL Server',
     'PostgreSQL',
+    'AWS',
+    'Azure',
+    'GCP',
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Kubernetes',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Chewy',
+      position: 'Senior Software Engineer',
+      from: 'Jan 2021',
+      to: 'Oct 2024',
+      companyLink: 'https://www.chewy.com/',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Insight Global',
+      position: 'Full Stack Engineer',
+      from: 'Jan 2016',
+      to: 'Dec 2020',
+      companyLink: 'https://insightglobal.com/',
+    },
+    {
+      company: 'Citadel',
+      position: 'Full Stack Developer',
+      from: 'Jun 2014',
+      to: 'Dec 2015',
+      companyLink: 'https://www.citadel.com/',
     },
   ],
   certifications: [
-    {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
-    },
+    // {
+    //   name: 'Lorem ipsum',
+    //   body: 'Lorem ipsum dolor sit amet',
+    //   year: 'March 2022',
+    //   link: 'https://example.com',
+    // },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
+      institution: 'Southern Methodist University',
+      degree: `Bachelor's Degree in Computer Science`,
+      from: '2010',
       to: '2014',
     },
   ],
   publications: [
-    {
-      title: 'Publication Title',
-      conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
+    // {
+    //   title: 'Publication Title',
+    //   conferenceName: '',
+    //   journalName: 'Journal Name',
+    //   authors: 'John Doe, Jane Smith',
+    //   link: 'https://example.com',
+    //   description:
+    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    // },
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: 'medium', // medium | dev
+    username: 'smartjoker', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    // id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
-    snippetVersion: 6,
+    // id: '',
+    // snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'night',
 
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
     disableSwitch: false,
 
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
     respectPrefersColorScheme: false,
 
-    // Display the ring in Profile picture
     displayAvatarRing: true,
 
-    // Available themes. To remove any theme, exclude from here.
     themes: [
       'light',
       'dark',
@@ -221,7 +221,6 @@ const CONFIG = {
       'procyon',
     ],
 
-    // Custom theme, applied to `procyon` theme
     customTheme: {
       primary: '#fc055b',
       secondary: '#219aaf',
@@ -233,7 +232,6 @@ const CONFIG = {
     },
   },
 
-  // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
       class="text-primary" href="https://github.com/arifszn/gitprofile"
       target="_blank"
